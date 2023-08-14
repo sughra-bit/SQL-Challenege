3e16337511bff10d92b3d1428a5dd10d48590f84
sughra-bit <135158002+sughra-bit@users.noreply.github.com>
2023-08-14 07:30:51 -0400

    Add files via upload

--- EmployeeSQL/Schema.sql
+++ EmployeeSQL/Schema.sql
@@ -8,7 +8,7 @@ SELECT * FROM departments;
 
 --Creating a table for "titles"
 CREATE TABLE titles(
-	title_id VARCHAR(5) PRIMARY KEY,
+	title_id VARCHAR(15) PRIMARY KEY,
 	title VARCHAR(30)
 );	
 SELECT * FROM titles
@@ -16,7 +16,7 @@ SELECT * FROM titles
 
 --creating a table for the employees
 CREATE TABLE employees(
-	emp_no VARCHAR(10) PRIMARY KEY,
+	emp_no INTEGER PRIMARY KEY,
 	emp_title VARCHAR(15),
 	birth_date VARCHAR(10),
 	first_name VARCHAR(30),
@@ -30,7 +30,7 @@ SELECT * FROM employees
 
 --Creating the table for "department employees"
 CREATE TABLE dept_emp(
-	emp_no VARCHAR(10) NOT NULL,
+	emp_no INTEGER,
 	dept_no VARCHAR(15) NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
 	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
@@ -42,7 +42,7 @@ SELECT * FROM dept_emp
 --Creating a table for "department managers"
 CREATE TABLE dept_manager(
 	dept_no VARCHAR(15),
-	emp_no VARCHAR(10),
+	emp_no INTEGER,
 	FOREIGN KEY(emp_no) REFERENCES employees(emp_no),
 	FOREIGN KEY(dept_no) REFERENCES departments(dept_no),
 	PRIMARY KEY(dept_no, emp_no)
@@ -52,7 +52,7 @@ SELECT * FROM dept_manager
 
 --creating a table "salaries"
 CREATE TABLE salaries(
-	emp_no VARCHAR(10),
+	emp_no INTEGER,
 	salary FLOAT,
 	FOREIGN KEY(emp_no) REFERENCES employees(emp_no)
 );
