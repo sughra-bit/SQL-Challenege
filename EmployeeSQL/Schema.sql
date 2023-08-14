10a51196a62cc954d73648e4b57dc5d0cb7cc814
sughra-bit <135158002+sughra-bit@users.noreply.github.com>
2023-08-14 06:57:02 -0400

    Add files via upload

---
+++ EmployeeSQL/Schema.sql
@@ -0,0 +1,59 @@
+--Creating the table "departments"
+CREATE TABLE departments(
+	dept_no VARCHAR(5) PRIMARY KEY,
+	dept_name VARCHAR(30)
+);
+SELECT * FROM departments;
+
+
+--Creating a table for "titles"
+CREATE TABLE titles(
+	title_id VARCHAR(5) PRIMARY KEY,
+	title VARCHAR(30)
+);	
+SELECT * FROM titles
+
+
+--creating a table for the employees
+CREATE TABLE employees(
+	emp_no VARCHAR(10) PRIMARY KEY,
+	emp_title VARCHAR(15),
+	birth_date VARCHAR(10),
+	first_name VARCHAR(30),
+	last_name VARCHAR(30),
+	sex VARCHAR(6),
+	hire_date VARCHAR(15),
+	FOREIGN KEY (emp_title) REFERENCES titles(title_id)
+);
+SELECT * FROM employees
+
+
+--Creating the table for "department employees"
+CREATE TABLE dept_emp(
+	emp_no VARCHAR(10) NOT NULL,
+	dept_no VARCHAR(15) NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+	PRIMARY KEY (emp_no, dept_no)
+);
+SELECT * FROM dept_emp
+
+
+--Creating a table for "department managers"
+CREATE TABLE dept_manager(
+	dept_no VARCHAR(15),
+	emp_no VARCHAR(10),
+	FOREIGN KEY(emp_no) REFERENCES employees(emp_no),
+	FOREIGN KEY(dept_no) REFERENCES departments(dept_no),
+	PRIMARY KEY(dept_no, emp_no)
+);
+SELECT * FROM dept_manager
+
+
+--creating a table "salaries"
+CREATE TABLE salaries(
+	emp_no VARCHAR(10),
+	salary FLOAT,
+	FOREIGN KEY(emp_no) REFERENCES employees(emp_no)
+);
+SELECT * FROM salaries
